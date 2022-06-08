2b4bfbd500292a83ab12c5446f875137a502e2d6
Sara Vieira <hey@iamsaravieira.com>
2022-06-08 15:07:19 +0300

    add hook

--- src/Cupcake.js
+++ src/Cupcake.js
@@ -1,4 +1,9 @@
+import { useFrosting } from ".";
+
 const Side = () => {
+  const { frosting } = useFrosting();
+
+  console.log(frosting);
   return (
     <div className="flex items-center justify-center h-full text-6xl">🧁</div>
   );
